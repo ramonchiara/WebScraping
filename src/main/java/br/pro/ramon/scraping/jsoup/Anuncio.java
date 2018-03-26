8069559083c7e839dba2a2d05e42d53bfb984a74
Ramon Chiara <ramonchiara@gmail.com>
2018-03-26 02:45:53 -0300

    Tentando baixar todos os imóveis de SP (100 páginas?)

--- src/main/java/br/pro/ramon/scraping/jsoup/Anuncio.java
+++ src/main/java/br/pro/ramon/scraping/jsoup/Anuncio.java
@@ -1,47 +1,76 @@
 package br.pro.ramon.scraping.jsoup;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Anuncio {
 
     private String url;
-    private String id;
+    private String breadcrumb;
     private String titulo;
+    private String data;
     private Double preco;
+    private String user;
+    private String descricao;
+    private Map<String, String> detalhes;
     private String cep;
+    private String codigo;
 
-    public Anuncio(String url, String id, String titulo, Double preco, String cep) {
+    public Anuncio(String url, String breadcrumb, String titulo, String data, Double preco, String user, String descricao, String cep, String codigo) {
         this.url = url;
-        this.id = id;
+        this.breadcrumb = breadcrumb;
         this.titulo = titulo;
+        this.data = data;
         this.preco = preco;
+        this.user = user;
+        this.descricao = descricao;
+        this.detalhes = new HashMap<>();
         this.cep = cep;
+        this.codigo = codigo;
     }
 
     public String getUrl() {
         return url;
     }
 
-    public String getId() {
-        return id;
+    public String getBreadcrumb() {
+        return breadcrumb;
     }
 
     public String getTitulo() {
         return titulo;
     }
 
+    public String getData() {
+        return data;
+    }
+
     public Double getPreco() {
         return preco;
     }
 
-    public String getPrecoAsString() {
-        return preco == null ? "" : preco.toString();
+    public String getUser() {
+        return user;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public String put(String key, String value) {
+        return detalhes.put(key, value);
+    }
+
+    public Map<String, String> getDetalhes() {
+        return detalhes;
     }
 
     public String getCep() {
         return cep;
     }
 
-    public boolean isBaixo() {
-        return preco != null && preco < 2000;
+    public String getCodigo() {
+        return codigo;
     }
 
 }
--- src/main/java/br/pro/ramon/scraping/jsoup/Program.java
+++ src/main/java/br/pro/ramon/scraping/jsoup/Program.java
@@ -11,8 +11,6 @@ import com.mongodb.client.MongoDatabase;
 import java.io.IOException;
 import java.text.NumberFormat;
 import java.text.ParseException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Locale;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -24,37 +22,46 @@ public class Program {
     public static void main(String[] args) throws IOException {
         ObjectMapper mapper = new ObjectMapper();
         mapper.enable(SerializationFeature.INDENT_OUTPUT);
-        // mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
         mapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
-
-        List<DBObject> dados = new ArrayList<>();
-
-        String ua = "Mozilla/5.0";
-        Document document = Jsoup.connect("http://sp.olx.com.br/imoveis").userAgent(ua).get();
-        Elements anuncios = document.select("#main-ad-list li.item");
-        for (Element anuncio : anuncios) {
-            String url = anuncio.select("a").attr("abs:href");
-            if (url.trim().isEmpty()) {
-                continue;
-            }
-
-            Document apto = Jsoup.connect(url).userAgent(ua).get();
-            String id = apto.select(".OLXad-id .description").text();
-            String titulo = apto.select("#ad_title").text();
-            String preco = apto.select(".OLXad-price").text();
-            String cep = apto.select("[class*=OLXad-location] p:contains(CEP do imóvel)").first().child(1).text();
-
-            Anuncio a = new Anuncio(url, id, titulo, getValor(preco), cep);
-            String json = mapper.writeValueAsString(a);
-
-            DBObject dado = BasicDBObject.parse(json);
-            dados.add(dado);
-        }
 
         MongoClient mongo = new MongoClient("localhost", 27017);
         MongoDatabase db = mongo.getDatabase("piv");
         MongoCollection<DBObject> table = db.getCollection("anuncios", DBObject.class);
-        table.insertMany(dados);
+
+        String ua = "Mozilla/5.0";
+        for (int pagina = 1; pagina <= 100; pagina++) {
+            Document document = Jsoup.connect("http://sp.olx.com.br/imoveis?o=" + pagina).userAgent(ua).get();
+            Elements anuncios = document.select("#main-ad-list li.item");
+            for (Element anuncio : anuncios) {
+                String url = anuncio.select("a").attr("abs:href");
+                if (url.trim().isEmpty()) {
+                    continue;
+                }
+
+                Document apto = Jsoup.connect(url).userAgent(ua).get();
+                String breadcrumb = apto.select(".breadcrumb-list").text();
+                String titulo = apto.select("#ad_title").text();
+                String data = apto.select(".OLXad-date").text();
+                String preco = apto.select(".OLXad-price").text();
+                String user = apto.select(".section_OLXad-user-info .owner").text();
+                String descricao = apto.select(".OLXad-description").text();
+                Elements detalhes = apto.select(".OLXad-details ul.list li");
+                String cep = apto.select("[class*=OLXad-location] p:contains(CEP do imóvel)").first().child(1).text();
+                String codigo = apto.select(".OLXad-id .description").text();
+
+                Anuncio a = new Anuncio(url, breadcrumb, titulo, data, getValor(preco), user, descricao, cep, codigo);
+
+                for (Element detalhe : detalhes) {
+                    String chave = detalhe.select(".term").text();
+                    String valor = detalhe.select(".description").text();
+                    a.put(chave, valor);
+                }
+
+                String json = mapper.writeValueAsString(a);
+                DBObject dado = BasicDBObject.parse(json);
+                table.insertOne(dado);
+            }
+        }
     }
 
     private static Double getValor(String preco) {
@@ -62,7 +69,7 @@ public class Program {
 
         try {
             if (preco != null && !preco.trim().isEmpty()) {
-                NumberFormat format = NumberFormat.getCurrencyInstance(Locale.getDefault()); // Locale.forLanguageTag("pt-BR")
+                NumberFormat format = NumberFormat.getCurrencyInstance(Locale.forLanguageTag("pt-BR")); // Locale.getDefault()
                 valor = format.parse(preco.trim()).doubleValue();
             }
         } catch (ParseException ex) {
