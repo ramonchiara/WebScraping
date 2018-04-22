1cbd1c85f63af41b6b5f2dce0a47451ce34aa637
Ramon Chiara <ramonchiara@gmail.com>
2018-04-21 21:12:26 -0300

    Alterações para não duplicar dados na base

--- src/main/java/br/pro/ramon/scraping/jsoup/Program.java
+++ src/main/java/br/pro/ramon/scraping/jsoup/Program.java
@@ -9,9 +9,12 @@ import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import java.io.IOException;
+import java.net.UnknownHostException;
 import java.text.NumberFormat;
 import java.text.ParseException;
 import java.util.Locale;
+import org.bson.BsonDocument;
+import org.bson.BsonString;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -28,6 +31,10 @@ public class Program {
         MongoDatabase db = mongo.getDatabase("piv");
         MongoCollection<DBObject> table = db.getCollection("anuncios", DBObject.class);
 
+        int novos = 0;
+        int antigos = 0;
+        int erros = 0;
+
         String ua = "Mozilla/5.0";
         for (int pagina = 1; pagina <= 100; pagina++) {
             Document document = Jsoup.connect("http://sp.olx.com.br/imoveis?o=" + pagina).userAgent(ua).get();
@@ -38,30 +45,50 @@ public class Program {
                     continue;
                 }
 
-                Document apto = Jsoup.connect(url).userAgent(ua).get();
-                String breadcrumb = apto.select(".breadcrumb-list").text();
-                String titulo = apto.select("#ad_title").text();
-                String data = apto.select(".OLXad-date").text();
-                String preco = apto.select(".OLXad-price").text();
-                String user = apto.select(".section_OLXad-user-info .owner").text();
-                String descricao = apto.select(".OLXad-description").text();
-                Elements detalhes = apto.select(".OLXad-details ul.list li");
-                String cep = apto.select("[class*=OLXad-location] p:contains(CEP do imóvel)").first().child(1).text();
-                String codigo = apto.select(".OLXad-id .description").text();
-
-                Anuncio a = new Anuncio(url, breadcrumb, titulo, data, getValor(preco), user, descricao, cep, codigo);
-
-                for (Element detalhe : detalhes) {
-                    String chave = detalhe.select(".term").text();
-                    String valor = detalhe.select(".description").text();
-                    a.put(chave, valor);
+                System.out.print(url);
+                DBObject d = table.find(new BsonDocument("url", new BsonString(url))).first();
+                if (d != null) {
+                    antigos++;
+                    System.out.println("... found!");
+                    continue;
+                } else {
+                    novos++;
+                    System.out.println("... downloading...");
                 }
+                try {
+                    Document apto = Jsoup.connect(url).userAgent(ua).get();
+                    String breadcrumb = apto.select(".breadcrumb-list").text();
+                    String titulo = apto.select("#ad_title").text();
+                    String data = apto.select(".OLXad-date").text();
+                    String preco = apto.select(".OLXad-price").text();
+                    String user = apto.select(".section_OLXad-user-info .owner").text();
+                    String descricao = apto.select(".OLXad-description").text();
+                    Elements detalhes = apto.select(".OLXad-details ul.list li");
+                    String cep = apto.select("[class*=OLXad-location] p:contains(CEP do imóvel)").first().child(1).text();
+                    String codigo = apto.select(".OLXad-id .description").text();
 
-                String json = mapper.writeValueAsString(a);
-                DBObject dado = BasicDBObject.parse(json);
-                table.insertOne(dado);
+                    Anuncio a = new Anuncio(url, breadcrumb, titulo, data, getValor(preco), user, descricao, cep, codigo);
+
+                    for (Element detalhe : detalhes) {
+                        String chave = detalhe.select(".term").text();
+                        String valor = detalhe.select(".description").text();
+                        a.put(chave, valor);
+                    }
+
+                    String json = mapper.writeValueAsString(a);
+                    DBObject dado = BasicDBObject.parse(json);
+                    table.insertOne(dado);
+                } catch (UnknownHostException ex) {
+                    novos--;
+                    erros++;
+                    System.err.println("A URL encontrada não é válida!");
+                }
             }
         }
+
+        System.out.println("Antigos: " + antigos);
+        System.out.println("Novos..: " + novos);
+        System.out.println("Erros..: " + erros);
     }
 
     private static Double getValor(String preco) {
