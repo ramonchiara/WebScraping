5857ee1683fd06329eeaf027830691e64af4552a
Ramon Chiara <ramonchiara@gmail.com>
2018-04-24 01:00:59 -0300

    Usando a biblioteca "certa" para converter o JSON em objeto.

--- src/main/java/br/pro/ramon/scraping/valor/Program.java
+++ src/main/java/br/pro/ramon/scraping/valor/Program.java
@@ -1,7 +1,7 @@
 package br.pro.ramon.scraping.valor;
 
-import com.mongodb.BasicDBObject;
-import com.mongodb.DBObject;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
@@ -12,10 +12,12 @@ import org.jsoup.select.Elements;
 public class Program {
 
     public static void main(String[] args) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+
         for (int page = 0; page < 5; page++) {
             String json = Jsoup.connect("http://www.valor.com.br/busca/gasolina?method=ajax&page=" + page).method(Connection.Method.POST).execute().body();
-            DBObject obj = BasicDBObject.parse(json);
-            String html = (String) obj.get("result");
+            JsonNode obj = mapper.readTree(json);
+            String html = obj.get("result").asText();
 
             Document document = Jsoup.parse(html);
             Elements noticias = document.select(".search-result-item");
